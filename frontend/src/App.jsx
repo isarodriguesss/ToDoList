b7439503be7ab58025149a7e25b5415093615c57
Isadora Barros Rodrigues <isadora.rodrigues70@gmail.com>
2023-02-09 11:21:23 -0300

    starting crud

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { nanoid } from 'nanoid';
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(false)
   const [todoList, setTodoList] = useState([])
 
   const [formData, setFormData] = useState(
@@ -18,29 +18,58 @@ export default function App() {
     
   },[count])
 
-  function handleIsCompleted(event, id) {
+  function handleUpdate(event, id) {
+    const {name, value, type, checked} = event.target
     const updateTodoList = todoList.map(item => {
       return item.id === id? {
         ...item, 
-        [event.target.name]: event.target.checked
+        [name]: type === "checkbox" ? checked : value
       } : item
     })
     setTodoList(updateTodoList)
+    const element = updateTodoList.find(item => item.id == id)
+    axios.put(`http://localhost:8000/api/todos/${id}/`, element)
+    //axios.post("http://localhost:8000/api/todos/", element)
   }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(todoList)
+  }
+
+  function handleDelete(id) {
+    axios.delete(`http://localhost:8000/api/todos/${id}/`)
+    setCount(!count)
+  }
+
+  /* function handleCreate {
+
+  } */
 
   const todoElements = todoList.map(item => (
     <div key={nanoid()}>
-      <p>{item.title}: {item.description}</p>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={item.title}
+          onChange={(e) => handleUpdate(e, item.id)}
+          name="title"
+        />
+        <textarea
+          value={item.description}
+          onChange={(e) => handleUpdate(e, item.id)}
+          name="description"
+        />
         <input 
           type='checkbox'
           checked={item.completed}
           id='completed'
-          onChange={(e) => handleIsCompleted(e, item.id)}
+          onChange={(e) => handleUpdate(e, item.id)}
           name='completed'
-          />
-          <label htmlFor='completed'> Task completed?</label>
+        />
+        <label htmlFor='completed'> Task completed?</label>
+        <button>Submit</button>
       </form>
+      <button onClick={() => handleDelete(item.id)}>Delete</button>
     </div>
     
   ))
